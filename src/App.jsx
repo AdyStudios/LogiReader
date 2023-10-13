fa2cb77cce8a3ce47e9385533b5239a3235f9edf
AdyStudios <karaadamka@gmail.com>
2023-10-13 17:46:51 +0200

    Teszt change

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,7 @@ import "./i18n";
 import { Trans } from "react-i18next";
 import "./components/Navigation";
 import Navigation from "./components/Navigation";
+//ez egy fontos kód részlet
 //import Content from "./example.json";
 
 
